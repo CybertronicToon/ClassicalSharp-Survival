1bd019993009dc7d58f3cf421f0ae89ba1f6f675
CybertronicToon <rexyrexy2@gmail.com>
2018-10-23 15:12:48 -0500

    Uploaded InteroPatcher.

---
+++ InteropPatcher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InteropPatcher {
+	
+	public static class Program {
+
+		public static int Main( string[] args ) {
+			try {
+				if( args.Length == 0 ) {
+					Console.WriteLine( "Expecting single argument specifying the file to patch" );
+					return 2;
+				}
+				// Some older IDEs seem to like splitting the path when it has spaces.
+				// So we undo this and treat the arguments as a single path.
+				string path = String.Join( " ", args );
+				Patcher.PatchFile( path );
+				return 0;
+			} catch( Exception ex ) {
+				Console.WriteLine( ex );
+				return 1;
+			}
+		}
+	}
+}
